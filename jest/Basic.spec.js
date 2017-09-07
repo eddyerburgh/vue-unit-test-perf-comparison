1039e1243d780f6a6a40dcb1c8358af844383f2a
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-09-07 07:52:43 +0100

    feat: add jest specs

---
+++ jest/Basic.spec.js
@@ -0,0 +1,154 @@
+import { shallow } from 'vue-test-utils'
+import Basic from '../components/Basic.vue'
+
+describe('Basic.vue', () => {
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+
+  it('displays correct test', () => {
+    const wrapper = shallow(Basic)
+    expect(wrapper.find('.hello h1').text()).toBe('Welcome to Your Vue.js App')
+  })
+})
---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+declare -a test_runners=( "jest" )
+
+for test_runner in "${test_runners[@]}"
+do (
+  cd "${test_runner}" || echo "failed to change into ./${test_runner}"
+  for i in {0..10}
+  do
+     cp Basic.spec.js "Basic$i.spec.js"
+  done
+  yarn test
+  for i in {0..10}
+  do
+     rm "Basic$i.spec.js"
+  done
+  )
+done
